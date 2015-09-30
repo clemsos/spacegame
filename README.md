d28d574987d085013e04db3134cdfa7b6d958abc
Clément Renaud <clement.renaud@gmail.com>
2015-09-30 06:37:49 +0200

    basic IO testing

--- README.md
+++ README.md
@@ -12,6 +12,20 @@ A game from outer space
     . venv/bin/activate
     pip install -r requirements.txt
 
+## Test
+
+    pip install -r dev_requirements.txt
+    py.test tests
+
+## Deploy
+
+You can deploy using my [flask-fabric-deploy](https://github.com/clemsos/flask-fabric-deploy) scripts.
+
+    git clone git@github.com:clemsos/flask-fabric-deploy.git
+    # edit the config/servers.py
+    fab prod setup 
+    fab prod deploy
+
 
 
 ## Credits
---
+++ tests/test_db.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from server.db import load_last_score, save_score, get_best
+
+class TestServerDb(unittest.TestCase):
+
+    def test_save_score(self):
+        """Score should be saved properly"""
+        save_score(11)
+        score = load_last_score()
+        self.assertTrue(score == 11)
+
+    def test_load_last_score(self): 
+        """ Latest score should be loaded """
+        save_score(11)
+        score = load_last_score()
+        self.assertTrue(score == 11)
+        save_score(4)
+        score = load_last_score()
+        self.assertTrue(score == 4)
+
+    def test_get_best(self):
+        """ Best score should be kept """
+        save_score(11)
+        best = get_best()
+        self.assertTrue(best ==11)
+        save_score(15)
+        best = get_best()
+        self.assertTrue(best ==15)
+        save_score(2)
+        best = get_best()
+        self.assertTrue(best ==15)
